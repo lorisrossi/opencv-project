1a7f44c18ef1cc8b3e05b5a8da9e57ebfb9167c6
Loris Rossi <lorisrossirn@gmail.com>
2018-05-26 10:35:31 +0200

    Add #include guard to hog.hpp

--- lib/hog.hpp
+++ lib/hog.hpp
@@ -1,4 +1,9 @@
+#ifndef HOG_HPP
+#define HOG_HPP
+
 #include <vector>
 #include "opencv2/objdetect.hpp"
 
 void computeHOG(const std::vector<cv::Mat> &imgs, std::vector<cv::Mat> &gradients);
+
+#endif
