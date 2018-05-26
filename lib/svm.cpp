ae29be7e4847bc487e5201eb374b47a515438717
Patrick Jusic <patrick-96@live.it>
2018-05-26 12:38:33 +0200

    Tunes svm parameters (best found)

--- lib/svm.cpp
+++ lib/svm.cpp
@@ -13,17 +13,14 @@ using namespace cv::ml;
 
 void initSvm(Ptr<SVM> &svm) {
   svm = SVM::create();
-  svm->setCoef0(0.0);
-  svm->setDegree(4);
+  svm->setCoef0(10.0);
+  svm->setDegree(6);
   svm->setTermCriteria(
       TermCriteria(CV_TERMCRIT_ITER + CV_TERMCRIT_EPS, 1000, 1e-3));
   svm->setGamma(3);
-  // svm->setKernel(SVM::LINEAR);
   svm->setKernel(SVM::POLY);
   svm->setNu(0.5);
-  svm->setP(0.1);  // for EPSILON_SVR, epsilon in loss function?
-  svm->setC(0.01); // From paper, soft classifier
-  // svm->setType(SVM::EPS_SVR);  // C_SVC; // EPSILON_SVR; // may be also
-  // NU_SVR;
-  svm->setType(SVM::C_SVC);
+  svm->setP(0.1);
+  svm->setC(0.01);
+  svm->setType(SVM::NU_SVC);
 }
